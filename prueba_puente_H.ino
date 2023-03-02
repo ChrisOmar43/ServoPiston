f145d5065e410f9db7665b9d1fdfd347df228a61
ChrisOmar43 <chrisomar08@hotmail.com>
2023-03-01 22:34:45 -0600

    Puente H

---
+++ prueba_puente_H.ino
@@ -0,0 +1,24 @@
+int velocidad = 255;
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6, OUTPUT);
+  analogWrite(3, velocidad);
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+  digitalWrite(5, HIGH);
+  delay(250);
+  digitalWrite(2,LOW);
+  digitalWrite(5, LOW);
+  delay(200);
+  digitalWrite(4,HIGH);
+  digitalWrite(6, HIGH);
+  delay(250);
+  digitalWrite(4, LOW);
+  digitalWrite(6, LOW);
+  delay(200);
+}
