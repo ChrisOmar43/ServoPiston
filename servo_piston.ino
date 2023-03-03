ba3a3ef29060e558e7e8f0d4e708778581bcca8e
ChrisOmar43 <chrisomar08@hotmail.com>
2023-03-03 00:04:10 -0600

    Funciones corregidas

--- servo_piston.ino
+++ servo_piston.ino
@@ -1,8 +1,21 @@
 int cm2 = 0;
-String pos = "inicio";
-String pos2 = "";  
-void movimiento(int p1, int p2, int d, int v);
-void ciclo(int cm);
+int pos2 = 0;  
+void movimiento(int p1, int p2, int d, int v){
+      analogWrite(3, v);
+      digitalWrite(p1,HIGH);
+      digitalWrite(p2, HIGH);
+      delay(d);
+      digitalWrite(p1, LOW);
+      digitalWrite(p2, LOW);
+}
+void ciclo(int cm){
+  for (int i = cm; i <= -1; i++) {
+    movimiento(2, 5, 500, 30);
+  }
+  for (int i = cm; i >= 1; i--) {
+    movimiento(6, 4, 620, 50);
+  }
+}
 
 void setup() {
   pinMode(2, OUTPUT);
@@ -15,34 +28,18 @@ void setup() {
 
 void loop() {
    if (Serial.available()) {
-    String pos = Serial.readString();
-    if (pos == "inicio"||pos == "calibrar"||pos == "0"){
-        movimiento(4,6,2540, 50);
-        pos = "0";
-        pos2 = "0";
+    int pos = Serial.parseInt();
+    if (pos == 404){
+        movimiento(2,5,2540, 50);
+        pos = 0;
+        pos2 = 0;
         cm2 = 0;
     }
-    else if (pos == "1"||pos == "2"||pos == "3"||pos == "4"){
-      cm2 = pos.toInt() - pos2.toInt();
+     if (pos == 1||pos == 2||pos == 3||pos == 4){
+      cm2 = pos - pos2;
       pos2 = pos;
+      Serial.println(cm2);
       ciclo(cm2);
     }
    }
-}
-
-void movimiento(int p1, int p2, int d, int v){
-      analogWrite(3, v);
-      digitalWrite(p1,HIGH);
-      digitalWrite(p2, HIGH);
-      delay(d);
-      digitalWrite(p1, LOW);
-      digitalWrite(p2, LOW);
-}
-void ciclo(int cm){
-  for (int i = cm; i >=  -1; i++) {
-    movimiento(4, 6, 635, 50);
-  }
-  for (int i = cm; i <=  1; i--) {
-    movimiento(2, 5, 650, 70);
-  }
 }
